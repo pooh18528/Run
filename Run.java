711d64730a51ad44bf3f8f5cad69f62b37bf4eed
pooh18528 <nitit828@gmail.com>
2021-10-29 13:53:23 +0700

    initial commit

---
+++ Run.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Run {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+         System.out.print("*****************");
+        System.out.print("You will answer 1 or 2.");
+         System.out.print("1) Run ///// 2) exit");
+          System.out.print("********************");
+        char floor = reader.next().charAt(0);
+
+        switch (floor) {
+         
+            case '1' :
+              int x = 1;
+              while (x <= 1000){
+                    System.out.print("7ewsghr84sze8g46b8drrgh edrzhykugk");
+                    System.out.print("rdhsz4h8tr4sdh8td38r4th8ntr4f4ht85h");
+                    System.out.print("szdh84edrhrhul4k398uo;447grdhjyrku");
+                    System.out.print("p[lk'ugkjsreg48d4j68tyf4j8t4xukl");
+              }
+            break;  
+            case '2' :
+                System.out.println("*********Goodbye********");
+                break;
+        
+        }
+
+    }
+}
